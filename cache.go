d46df7299d6dab01727c132b37c9e7cf70b02875
Timofei Liakhor <timofei.liakhor@gmail.com>
2022-05-25 21:40:18 +0400

    golang-united-school-homework-5.2

--- cache.go
+++ cache.go
@@ -3,21 +3,69 @@ package cache
 import "time"
 
 type Cache struct {
+	holder map[string]Object
+}
+
+type Object struct {
+	value    string
+	isTill   bool
+	deadline time.Time
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{
+		holder: make(map[string]Object),
+	}
 }
 
-func (receiver) Get(key string) (string, bool) {
+func (cache *Cache) Get(key string) (string, bool) {
+	value, isExist := cache.holder[key]
+	if isExist == false {
+		return "", isExist
 
+	} else {
+		if value.isTill == false {
+			return value.value, true
+		} else {
+			now := time.Now()
+
+			if now.After(value.deadline) || now.Equal(value.deadline) {
+				delete(cache.holder, key)
+				return "", false
+			}
+
+			return value.value, true
+		}
+	}
 }
 
-func (receiver) Put(key, value string) {
+func (cache *Cache) Put(key, value string) {
+	cache.holder[key] = Object{
+		value: value,
+	}
 }
 
-func (receiver) Keys() []string {
+func (cache *Cache) Keys() []string {
+	res := make([]string, 20)
+	for _, v := range cache.holder {
+		if v.isTill == false {
+			res = append(res, v.value)
+		} else {
+			now := time.Now()
+
+			if now.Before(v.deadline) {
+				res = append(res, v.value)
+			}
+		}
+	}
+
+	return res
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (cache *Cache) PutTill(key, value string, deadline time.Time) {
+	cache.holder[key] = Object{
+		value:    value,
+		isTill:   true,
+		deadline: deadline,
+	}
 }
